6318e3c3660799cc3023ddcfb7bcc1f2e3b0680f
1P9S7U7 <140295363+1P9S7U7@users.noreply.github.com>
2024-12-13 23:08:08 +0700

    unchange policy.py

--- policy.py
+++ policy.py
@@ -122,4 +122,4 @@ class GreedyPolicy(Policy):
                 if pos_x is not None and pos_y is not None:
                     break
 
-        return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
+        return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)} 
