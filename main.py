d248c4bc9646036749ed74246240757ff3dccd9a
Nguyen Khoa <lilkhoa2k5@gmail.com>
2024-12-13 13:06:30 +0700

    third

--- main.py
+++ main.py
@@ -8,38 +8,38 @@ env = gym.make(
     "gym_cutting_stock/CuttingStock-v0",
     render_mode="human",  # Comment this line to disable rendering
 )
-NUM_EPISODES = 1
+NUM_EPISODES = 100
 
 if __name__ == "__main__":
     # Reset the environment
-    # observation, info = env.reset(seed=42)
+    observation, info = env.reset(seed=42)
 
     # Test GreedyPolicy
-    # gd_policy = GreedyPolicy()
-    # ep = 0
-    # while ep < NUM_EPISODES:
-    #     action = gd_policy.get_action(observation, info)
-    #     observation, reward, terminated, truncated, info = env.step(action)
+    gd_policy = GreedyPolicy()
+    ep = 0
+    while ep < NUM_EPISODES:
+        action = gd_policy.get_action(observation, info)
+        observation, reward, terminated, truncated, info = env.step(action)
 
-    #     if terminated or truncated:
-    #         print(info)
-    #         observation, info = env.reset(seed=ep)
-    #         ep += 1
+        if terminated or truncated:
+            print(info)
+            observation, info = env.reset(seed=ep)
+            ep += 1
 
     # Reset the environment
-    # observation, info = env.reset(seed=42)
+    observation, info = env.reset(seed=42)
 
     # # Test RandomPolicy
-    # rd_policy = RandomPolicy()
-    # ep = 0
-    # while ep < NUM_EPISODES:
-    #     action = rd_policy.get_action(observation, info)
-    #     observation, reward, terminated, truncated, info = env.step(action)
+    rd_policy = RandomPolicy()
+    ep = 0
+    while ep < NUM_EPISODES:
+        action = rd_policy.get_action(observation, info)
+        observation, reward, terminated, truncated, info = env.step(action)
 
-    #     if terminated or truncated:
-    #         print(info)
-    #         observation, info = env.reset(seed=ep)
-    #         ep += 1
+        if terminated or truncated:
+            print(info)
+            observation, info = env.reset(seed=ep)
+            ep += 1
 
     # Uncomment the following code to test your policy
     # # Reset the environment
