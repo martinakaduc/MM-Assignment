e9179bbe4ccf4a488abe9456cdf0c1f8babe4343
NguyenThanhTrinh275 <trinh020705@gmail.com>
2024-12-13 10:35:31 +0700

    Change neighbor solution of SA

--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -11,8 +11,7 @@ class SimulatedAnnealing(Policy):
         self.iterations = iterations
 
     def get_action(self, observation, info):
-        ffd_policy = FFD()
-        initial_action = ffd_policy.get_action(observation, info)
+        initial_action = self.FFD_action(observation, info)
 
         if initial_action is None:
             return self.get_default_action()
@@ -51,62 +50,10 @@ class SimulatedAnnealing(Policy):
                 break
 
         return best_action
-
-    def generate_neighbor(self, action, observation):
-        neighbor = action.copy()
-
-        move_or_rotate = random.choice(['move', 'rotate'])
-
-        if move_or_rotate == 'move':
-            new_x = max(0, action['position'][0] + random.randint(-1, 1))
-            new_y = max(0, action['position'][1] + random.randint(-1, 1))
-            neighbor['position'] = (new_x, new_y)
-        elif move_or_rotate == 'rotate':
-            neighbor['size'] = (action['size'][1], action['size'][0])
-
-        if self.is_valid_action(neighbor, observation):
-            return neighbor
-        else:
-            return None  # Không hợp lệ, bỏ qua
-
-    def is_valid_action(self, action, observation):
-        stock = observation["stocks"][action["stock_idx"]]
-        stock_w, stock_h = self._get_stock_size_(stock)
-        pos_x, pos_y = action["position"]
-        prod_w, prod_h = action["size"]
-
-        # Kiểm tra giới hạn của stock
-        if pos_x + prod_w > stock_w or pos_y + prod_h > stock_h:
-            return False
-        return self._can_place_(stock, (pos_x, pos_y), action["size"])
-
-    def calculate_cost(self, action, observation):
-        stock = observation["stocks"][action["stock_idx"]].copy() 
-        pos_x, pos_y = action["position"]
-        prod_w, prod_h = action["size"]
-
-        if pos_x + prod_w > stock.shape[1] or pos_y + prod_h > stock.shape[0]:
-            return float('inf') 
-
-        if not self._can_place_(stock, (pos_x, pos_y), (prod_w, prod_h)):
-            return float('inf')  
-
-        stock[pos_y: pos_y + prod_h, pos_x: pos_x + prod_w] = 1  
-        remaining_empty_area = np.sum(stock == -1)
-
-        return remaining_empty_area
-
-    def get_default_action(self):
-        return {"stock_idx": 0, "size": [1, 1], "position": (0, 0)}
-
-class FFD(Policy):
-    def __init__(self):
-        pass
-
-    def get_action(self, observation, info):
+    
+    def FFD_action(self, observation, info):
         list_prods = observation["products"]
 
-        # Sắp xếp sản phẩm theo diện tích giảm dần
         list_prods = sorted(list_prods, key=lambda x: x["size"][0] * x["size"][1], reverse=True)
 
         prod_size = [0, 0]
@@ -151,3 +98,68 @@ class FFD(Policy):
                     break
 
         return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
+
+    def generate_neighbor(self, action, observation):
+        neighbor = action.copy()
+
+        # Chọn ngẫu nhiên một stock
+        candidate_stock_idx = random.randint(0, len(observation["stocks"]) - 1)
+        neighbor["stock_idx"] = candidate_stock_idx
+
+        prod_w, prod_h = neighbor["size"]
+        prod_size = (prod_w, prod_h)
+
+        stock = observation["stocks"][candidate_stock_idx]
+        stock_w, stock_h = self._get_stock_size_(stock)
+
+        placed = False
+
+        if stock_w >= prod_w and stock_h >= prod_h:
+            for x in range(stock_w - prod_w + 1):
+                for y in range(stock_h - prod_h + 1):
+                    if self._can_place_(stock, (x, y), prod_size):
+                        neighbor["position"] = (x, y)
+                        placed = True
+                        return neighbor
+
+        if not placed and prod_w != prod_h and stock_w >= prod_h and stock_h >= prod_w:
+            rotated_size = (prod_h, prod_w)
+            for x in range(stock_w - prod_h + 1):
+                for y in range(stock_h - prod_w + 1):
+                    if self._can_place_(stock, (x, y), rotated_size):
+                        neighbor["size"] = rotated_size
+                        neighbor["position"] = (x, y)
+                        placed = True
+                        return neighbor
+
+        return None
+
+    def is_valid_action(self, action, observation):
+        stock = observation["stocks"][action["stock_idx"]]
+        stock_w, stock_h = self._get_stock_size_(stock)
+        pos_x, pos_y = action["position"]
+        prod_w, prod_h = action["size"]
+
+        if pos_x + prod_w > stock_w or pos_y + prod_h > stock_h:
+            return False
+        return self._can_place_(stock, (pos_x, pos_y), action["size"])
+
+    def calculate_cost(self, action, observation):
+        stock = observation["stocks"][action["stock_idx"]].copy() 
+        pos_x, pos_y = action["position"]
+        prod_w, prod_h = action["size"]
+
+        if pos_x + prod_w > stock.shape[1] or pos_y + prod_h > stock.shape[0]:
+            return float('inf') 
+
+        if not self._can_place_(stock, (pos_x, pos_y), (prod_w, prod_h)):
+            return float('inf')  
+        product_idx = action.get("product_idx", 0)
+
+        stock[pos_y: pos_y + prod_h, pos_x: pos_x + prod_w] = product_idx
+        remaining_empty_area = np.sum(stock == -1)
+
+        return remaining_empty_area
+
+    def get_default_action(self):
+        return {"stock_idx": 0, "size": [1, 1], "position": (0, 0)}
