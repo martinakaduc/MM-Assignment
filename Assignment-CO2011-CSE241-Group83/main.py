5b96411e14bca87e55f7d153c0827076920005e6
LENGUYENMINHDUAN <duan.lenguyenminh@hcmut.edu.vn>
2024-12-15 20:11:52 +0700

    Chinh sua main.py

--- Assignment-CO2011-CSE241-Group83/main.py
+++ Assignment-CO2011-CSE241-Group83/main.py
@@ -1,7 +1,7 @@
 import gym_cutting_stock
 import gymnasium as gym
 from policy import GreedyPolicy, RandomPolicy
-#from student_submissions.s2210xxx.policy2210xxx import Policy2210xxx
+from student_submissions.s2352590.policy2352590 import Policy2352590
 
 # Create the environment
 env = gym.make(
@@ -12,21 +12,7 @@ NUM_EPISODES = 100
 
 if __name__ == "__main__":
     # Reset the environment
-    # Uncomment the following code to test your policy
-    # Reset the environment
     observation, info = env.reset(seed=42)
-
-    policy2210xxx = Policy2210xxx(policy_id=2)
-    for _ in range(200):
-        action = policy2210xxx.get_action(observation, info)
-        observation, reward, terminated, truncated, info = env.step(action)
-
-
-        if terminated or truncated:
-            print(info)
-            observation, info = env.reset()
-
-"""   observation, info = env.reset(seed=42)
 
     # Test GreedyPolicy
     gd_policy = GreedyPolicy()
@@ -54,6 +40,19 @@ if __name__ == "__main__":
             print(info)
             observation, info = env.reset(seed=ep)
             ep += 1
-"""
+
+    # Uncomment the following code to test your policy
+    # # Reset the environment
+    # observation, info = env.reset(seed=42)
+    # print(info)
+
+    # policy2210xxx = Policy2210xxx(policy_id=1)
+    # for _ in range(200):
+    #     action = policy2210xxx.get_action(observation, info)
+    #     observation, reward, terminated, truncated, info = env.step(action)
+    #     print(info)
+
+    #     if terminated or truncated:
+    #         observation, info = env.reset()
 
 env.close()
