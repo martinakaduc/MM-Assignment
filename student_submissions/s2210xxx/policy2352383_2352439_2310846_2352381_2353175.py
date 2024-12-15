4d7aad1fae79a307e0aee5f1f713744c5aa1535d
Hoahoc11 <150876414+Hoahoc11@users.noreply.github.com>
2024-12-15 17:56:30 +0700

    Update policy2352383_2352439_2310846_2352381_2353175.py

--- student_submissions/s2210xxx/policy2352383_2352439_2310846_2352381_2353175.py
+++ student_submissions/s2210xxx/policy2352383_2352439_2310846_2352381_2353175.py
@@ -2,7 +2,7 @@ from policy import Policy
 from scipy.optimize import linprog
 import numpy as np
 
-class Policy2210xxx(Policy):
+class Policy2352383_2352439_2310846_2352381_2353175(Policy):
     def __init__(self, policy_id=1):
         assert policy_id in [1, 2], "Policy ID must be 1 or 2"
         self.policy_id = policy_id
