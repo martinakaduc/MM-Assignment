066dcbdc60b81c0838134c4a03d27b5e909c6ee0
LAPTOP-1QRLTJDB\DUC TIN <tin.hoduc1608@hcmut.edu.vn>
2024-12-16 20:12:54 +0700

    update

--- student_submissions/s2213486_2313771_2313334_2212896_2313431/policy2213486_2313771_2313334_2212896_2313431.py
+++ student_submissions/s2213486_2313771_2313334_2212896_2313431/policy2213486_2313771_2313334_2212896_2313431.py
@@ -1,125 +1,185 @@
-from policy import Policy
 import numpy as np
-class Policy2213332_2213996_2213851_2134006_2010545(Policy):
+from policy import Policy
+
+class Policy2213486_2313771_2313334_2212896_2313431(Policy):
     def __init__(self, policy_id=1):
-        assert policy_id in [1, 2 ,3], "Policy ID must be 1 or 2 or 3"
+        assert policy_id in [1, 2], "Policy ID must be 1 or 2"
         self.policy_id = policy_id
+        self.__list_of_sorted_products_indexes = []
+        self.__list_of_sorted_stocks_indexes = []
+        self.__list_of_stocks_used = []
+        self.__current_idx = 0
+        self._total_of_quantities = 0
+        self.__list_of_products_size = []
+        self.__list_of_stocks_size = []
+
+    def _reset_attributes(self):
+        self.__list_of_sorted_products_indexes = []
+        self.__list_of_sorted_stocks_indexes = []
+        self.__list_of_stocks_used = []
+        self.__current_idx = 0
+        self._total_of_quantities = 0
+        self.__list_of_products_size = []
+        self.__list_of_stocks_size = []
+
+    def _remaining_free_area(self, stock):
+        """
+        Empty area of stock.
+        """
+        return np.sum(stock == -1)
+
+    def _sum_of_quantities(self, list_of_product):
+        """
+        Find sum of quantities of products
+        """
+        result = 0
+        for prod in list_of_product:
+            result += prod["quantity"]
+        return result
+
+    def _get_stock_area(self, stock):
+        return np.sum(stock != -2)
+
+    def _copy_arr_size(self, arr_size):
+        result = []
+        for i in range(len(arr_size)):
+            result.append(arr_size[i][:])
+        return result
+
+    def _is_2_arr_size_same(self, arr1, arr2):
+        if len(arr1) != len(arr2):
+            return False
+        for i in range(len(arr1)):
+            a, b = arr1[i]
+            c, d = arr2[i]
+            if a != c or b != d:
+                return False
+        return True
 
     def get_action(self, observation, info):
         if self.policy_id == 1:
-            return self.near_optimal_cutting_action(observation)
+            return self.first_fit_decreasing(observation)
         elif self.policy_id == 2:
-            return self.two_dimensional_cutting_action(observation)
-        elif self.policy_id == 3:
-            return self.first_fit_decreasing_height_action(observation)
+            return self.best_fit_decreasing_height(observation)
 
-    def near_optimal_cutting_action(self, observation):
-        list_prods = observation["products"]  
-        stock_idx = -1  
-        pos_x, pos_y = None, None  
-
-        # Lọc các sản phẩm có số lượng > 0 và sắp xếp theo diện tích (từ lớn đến nhỏ)
-        prod_sizes = sorted(
-            [prod["size"] for prod in list_prods if prod["quantity"] > 0],
-            key=lambda x: x[0] * x[1],  
-            reverse=True,  
+    def first_fit_decreasing(self, observation):
+        products = sorted(
+            [prod for prod in observation["products"] if prod["quantity"] > 0],
+            key=lambda x: x["size"][0] * x["size"][1],  # Sort by area (width * height)
+            reverse=True
         )
-        
-        if not prod_sizes:
-            return {"stock_idx": -1, "size": [0, 0], "position": (0, 0)}
 
-        def fit_item_in_stock(stock, item):
-            stock_w, stock_h = self._get_stock_size_(stock)  
-            positions = []  
+        stocks = observation["stocks"]
 
-            # Kiểm tra các vị trí không xoay sản phẩm
-            for x in range(stock_w - item[0] + 1):
-                for y in range(stock_h - item[1] + 1):
-                    if self._can_place_(stock, (x, y), item):  
-                        positions.append((x, y, item))  
+        for product in products:
+            prod_width, prod_height = product["size"]
 
-            # Kiểm tra các vị trí với sản phẩm xoay 90 độ
-            rotated_item = (item[1], item[0])  
-            for x in range(stock_w - rotated_item[0] + 1):
-                for y in range(stock_h - rotated_item[1] + 1):
-                    if self._can_place_(stock, (x, y), rotated_item):  
-                        positions.append((x, y, rotated_item))  
+            for stock_idx, stock in enumerate(stocks):
+                stock_width, stock_height = self._get_stock_size_(stock)
+                free_area = self._remaining_free_area(stock)
 
-            # Chọn vị trí có diện tích dư thừa ít nhất
-            best_position = None
-            min_residual = float("inf")  
-            for pos in positions:
-                residual_space = (
-                    (stock_w - (pos[0] + pos[2][0])) * stock_h +
-                    (stock_h - (pos[1] + pos[2][1])) * stock_w
-                )  # Tính diện tích dư thừa
-                if residual_space < min_residual:  
-                    min_residual = residual_space
-                    best_position = pos
+                for orientation in [(prod_width, prod_height), (prod_height, prod_width)]:
+                    width, height = orientation
+                    if stock_width >= width and stock_height >= height and (width * height <= free_area):
+                        for x in range(stock_width - width + 1):
+                            for y in range(stock_height - height + 1):
+                                if self._can_place_(stock, (x, y), orientation):
+                                    return {
+                                        "stock_idx": stock_idx,
+                                        "size": orientation,
+                                        "position": (x, y)
+                                    }
 
-            return best_position  # Trả về vị trí tối ưu cho sản phẩm
+        return {"stock_idx": -1, "size": [0, 0], "position": (0, 0)}
+    def best_fit_decreasing_height(self, observation):
+        products = observation["products"]
+        list_of_products_size = [prod["size"] for prod in products]
+        stocks = observation["stocks"]
+        list_of_stocks_size = [self._get_stock_size_(stock) for stock in stocks]
 
-        # Duyệt qua tất cả các cuộn và các sản phẩm để tìm vị trí cắt
-        for i, stock in enumerate(observation["stocks"]):
-            for item in prod_sizes:
-                placement = fit_item_in_stock(stock, item)  
-                if placement:
-                    pos_x, pos_y, final_size = placement  
-                    return {"stock_idx": i, "size": final_size, "position": (pos_x, pos_y)}  
+        if (not self._is_2_arr_size_same(list_of_products_size, self.__list_of_products_size) or
+                not self._is_2_arr_size_same(list_of_stocks_size, self.__list_of_stocks_size) or
+                self._total_of_quantities != self._sum_of_quantities(products)):
+                self._reset_attributes()
+                self.__list_of_products_size = self._copy_arr_size(list_of_products_size)
+                self.__list_of_stocks_size = self._copy_arr_size(list_of_stocks_size)
 
-        return {"stock_idx": -1, "size": [0, 0], "position": (0, 0)}  
-        
-    def two_dimensional_cutting_action(self, observation):
-        list_prods = observation["products"]
-        stock_idx, pos_x, pos_y = -1, None, None
+        if not self.__list_of_sorted_products_indexes:
+            self.__list_of_sorted_products_indexes = sorted(
+                [i for i, product in enumerate(products)],
+                key=lambda i: products[i]["size"][0] * products[i]["size"][1], reverse=True
+            )
+            self._total_of_quantities += sum(product["quantity"] for product in products)
 
-        # Sắp xếp sản phẩm theo chiều cao giảm dần
-        sorted_prods = sorted(list_prods, key=lambda x: x["size"][1], reverse=True)
+        if not self.__list_of_sorted_stocks_indexes:
+            self.__list_of_sorted_stocks_indexes = sorted(
+                [i for i, stock in enumerate(stocks)],
+                key=lambda i: self._get_stock_size_(stocks[i])[0] * self._get_stock_size_(stocks[i])[1], reverse=True
+            )
 
-        for prod in sorted_prods:
-            if prod["quantity"] > 0:
-                prod_size = prod["size"]
-                for i, stock in enumerate(observation["stocks"]):
-                    stock_w, stock_h = self._get_stock_size_(stock)
+        idx = self.__list_of_sorted_products_indexes[self.__current_idx]
+        sum_of_quantities = self._sum_of_quantities(products)
 
-                    # Tìm vị trí cắt cho sản phẩm không xoay
-                    for x in range(stock_w - prod_size[0] + 1):
-                        for y in range(stock_h - prod_size[1] + 1):
-                            if self._can_place_(stock, (x, y), prod_size):
-                                return {"stock_idx": i, "size": prod_size, "position": (x, y)}
+        count_val = 0
+        while products[idx]["quantity"] == 0 and sum_of_quantities > 0:
+            if count_val == len(self.__list_of_sorted_products_indexes):
+                break
+            self.__current_idx += 1
+            if self.__current_idx >= len(self.__list_of_sorted_products_indexes):
+                self.__current_idx = 0
+            idx = self.__list_of_sorted_products_indexes[self.__current_idx]
+            count_val += 1
 
-                    # Kiểm tra sản phẩm xoay
-                    if stock_w >= prod_size[1] and stock_h >= prod_size[0]:
-                        for x in range(stock_w - prod_size[1] + 1):
-                            for y in range(stock_h - prod_size[0] + 1):
-                                if self._can_place_(stock, (x, y), prod_size[::-1]):
-                                    return {"stock_idx": i, "size": prod_size[::-1], "position": (x, y)}
+        product = products[idx]
+        product_size = product["size"]
 
-        return {"stock_idx": -1, "size": [0, 0], "position": (0, 0)}  # Không tìm thấy vị trí hợp lệ
-        
-    def first_fit_decreasing_height_action(self, observation):
-        """ Thuat toan First fit decreasing height (FFDH) """
-        list_prods = list(observation["products"])
-        list_prods.sort(key=lambda p: max(p["size"]), reverse=True)
+        best_stock_idx = -1
+        best_remain_area = float('inf')
+        best_position = None
+        best_orientation = None
 
-        for prod in list_prods:
-            if prod["quantity"] > 0:
-                prod_size = prod["size"]
+        for stock_idx in self.__list_of_stocks_used:
+            stock = stocks[stock_idx]
+            stock_width, stock_height = self._get_stock_size_(stock)
 
-                for stock_idx, stock in enumerate(observation["stocks"]):
-                    stock_w, stock_h = self._get_stock_size_(stock)
-                    prod_w, prod_h = prod_size
+            for orientation in [(product_size[0], product_size[1]), (product_size[1], product_size[0])]:
+                width, height = orientation
+                free_area = self._remaining_free_area(stock)
+                if stock_width >= width and stock_height >= height and (width * height <= free_area):
+                    for x in range(stock_width - width + 1):
+                        for y in range(stock_height - height + 1):
+                            if self._can_place_(stock, (x, y), orientation):
+                                remaining_area = free_area - (width * height)
+                                if remaining_area < best_remain_area:
+                                    best_remain_area = remaining_area
+                                    best_stock_idx = stock_idx
+                                    best_position = (x, y)
+                                    best_orientation = orientation
 
-                    # Tìm vị trí thấp nhất mà có thể đặt sản phẩm
-                    for y in range(stock_h - prod_h + 1):
-                        for x in range(stock_w - prod_w + 1):
-                            if self._can_place_(stock, (x, y), prod_size):
-                                return {"stock_idx": stock_idx, "size": prod_size, "position": (x, y)}
+        if best_stock_idx != -1:
+            self.__current_idx += 1
+            if self.__current_idx >= len(self.__list_of_sorted_products_indexes):
+                self.__current_idx = 0
+            if self._sum_of_quantities(observation["products"]) == 1:
+                self._reset_attributes()
+            self._total_of_quantities -= 1
+            return {
+                "stock_idx": best_stock_idx,
+                "size": best_orientation,
+                "position": best_position
+            }
 
-                            # Thử xoay ngược sản phẩm
-                            if self._can_place_(stock, (x, y), prod_size[::-1]):
-                                prod_size = prod_size[::-1]
-                                return {"stock_idx": stock_idx, "size": prod_size, "position": (x, y)}
+        next_stock = len(self.__list_of_stocks_used)
+        self.__list_of_stocks_used.append(next_stock)
 
-        # Nếu không thể đặt sản phẩm ở bất kỳ đâu, tạo vị trí ở level mới
-        return {"stock_idx": None, "size": prod_size, "position": (0, 0)}
+        self.__current_idx += 1
+        if self.__current_idx >= len(self.__list_of_sorted_products_indexes):
+            self.__current_idx = 0
+        if self._sum_of_quantities(observation["products"]) == 1:
+            self._reset_attributes()
+        self._total_of_quantities -= 1
+        return {
+            "stock_idx": next_stock,
+            "size": product_size,
+            "position": (0, 0)
+        }
