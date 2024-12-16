683251389921b889dbccce83cdcdfa91aa4922cf
Thai Toan <103176310+catunderrain@users.noreply.github.com>
2024-12-16 23:10:01 +0700

    update class name

--- student_submissions/s2311064_2311188_2311454_2311888_2311409/policy2311064_2311188_2311454_2311888_2311409.py
+++ student_submissions/s2311064_2311188_2311454_2311888_2311409/policy2311064_2311188_2311454_2311888_2311409.py
@@ -1,7 +1,7 @@
 from policy import Policy
 import numpy as np
 
-class Policy2210xxx(Policy):
+class Policy2311064_2311188_2311454_2311888_2311409(Policy):
     def __init__(self, policy_id=1):
         assert policy_id in [1, 2], "Policy ID must be 1 or 2"
 
